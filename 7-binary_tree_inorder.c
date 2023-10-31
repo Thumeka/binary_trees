31322412dd420212475f6680d0ed229e70138427
thumeka <thumekatembani21@gmail.com>
2023-10-31 11:14:42 -0700

    7

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - binary tree using in-order traversal
+ * @func: function
+ * @tree: root pointer
+ */
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+	func(tree->n);
+
+	binary_tree_inorder(tree->left, func);
+	binary_tree_inorder(tree->right, func);
+}
