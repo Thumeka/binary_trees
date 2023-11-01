6a5e00f275f7dcad8d2b05956ca074bad390128e
thumeka <thumekatembani21@gmail.com>
2023-11-01 06:33:04 -0700

    12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,13 +8,11 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t count_l = 0;
-
 	if (tree == NULL)
 		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
-	return (binary_tree_leaves(tree->left) + (binary_tree_leaves(tree->right)));
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
